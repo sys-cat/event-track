f936403449d221204404e00889a98909dafd4372
sys_cat <systemcat91@gmail.com>
2016-04-07 15:18:50 +0900

    modify sql

--- event.sql
+++ event.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS event;
 use event;
-CREATE TABLE if not exists `event.events` (
+CREATE TABLE if not exists `events` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(256) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE if not exists `event.pro_event` (
+CREATE TABLE if not exists `pro_event` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `rid` varchar(256) NOT NULL DEFAULT '',
   `event_id` varchar(256) NOT NULL DEFAULT '',
@@ -15,7 +15,7 @@ CREATE TABLE if not exists `event.pro_event` (
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE if not exists `event.stg_event` (
+CREATE TABLE if not exists `stg_event` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `rid` varchar(256) NOT NULL DEFAULT '',
   `event_id` varchar(256) NOT NULL DEFAULT '',
