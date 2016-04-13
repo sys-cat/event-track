42afcdc07f85e807f96138812338aca2e027c546
sys_cat <systemcat91@gmail.com>
2016-04-13 11:35:16 +0900

    add procces

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-export GOPATH="~/.go"
+export GOPATH=~/.go
 export GOROOT="/usr/local/go"
 export PATH="$GOROOT/bin:$PATH"
+cd ~/event_track
 $(go get github.com/gin-gonic/gin)
 $(go get github.com/BurntSushi/toml)
 $(go get github.com/go-sql-driver/mysql)
 $(mysql -u root < ./event.sql)
+$(go run ./main.go)
